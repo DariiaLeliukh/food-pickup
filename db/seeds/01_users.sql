bfbdf7ff6b43fc543b5b0ba0fab3a1007da4cba2
Ian Sebleius <sebeliusancira@gmail.com>
2023-08-20 20:10:43 -0700

    add restaurant_id to menu_items seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -11,17 +11,17 @@ VALUES
   ('Pizza Paradise', '123-456-789', 'info@pizzaparadise.com', '123 Main St, Vancouver', 'restaurant_image_url', '0900', '2200', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 5),
   ('Pasta Palace', '123-456-798', 'info@pastapalace.com', '1 St, Victoria', 'restaurant_image_url', '1000', '2100', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 4);
 
-INSERT INTO menu_items (name, price, description, image)
+INSERT INTO menu_items (restaurant_id, name, price, description, image)
 VALUES
-('PASTA', 15, 'Spaghetti bolognese - traditional italian pasta', 'https://media.istockphoto.com/id/544990588/photo/spaghetti-bolognese-on-dark-background.jpg?s=2048x2048&w=is&k=20&c=SBXtgdzzfO9uSuFoYskHG6fvNxBkwKDXw-XxU0g7Np0=');
+(1, 'PASTA', 15, 'Spaghetti bolognese - traditional italian pasta', 'https://media.istockphoto.com/id/544990588/photo/spaghetti-bolognese-on-dark-background.jpg?s=2048x2048&w=is&k=20&c=SBXtgdzzfO9uSuFoYskHG6fvNxBkwKDXw-XxU0g7Np0=');
 
-INSERT INTO menu_items (name, price, description, image)
+INSERT INTO menu_items (restaurant_id, name, price, description, image)
 VALUES
-('PIZZA', 20, 'Hawaiian Chicken PizzaSmoked', 'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1981&q=80');
+(1, 'PIZZA', 20, 'Hawaiian Chicken PizzaSmoked', 'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1981&q=80');
 
-INSERT INTO menu_items (name, price, description, image)
+INSERT INTO menu_items (restaurant_id, name, price, description, image)
 VALUES
-('LASAGNA', 18, 'Protein rich Lasagna Vegan Italian food plant-based recipe Mediterranean diet with ingredients such as Lasagna pasta, onion, mushrooms, zucchini, bell pepper, spinach, vegan marinara sauce, tomatoes, firm tofu, chickpeas, vegan cheese', 'https://media.istockphoto.com/id/1365127906/photo/protein-rich-lasagna-vegan-italian-food-plant-based-recipe-medit.jpg?s=2048x2048&w=is&k=20&c=RaSMjPx9vS_6i0_mpOC0CdD9LMmkW_NlCWOeqFWYSvg=');
+(2, 'LASAGNA', 18, 'Protein rich Lasagna Vegan Italian food plant-based recipe Mediterranean diet with ingredients such as Lasagna pasta, onion, mushrooms, zucchini, bell pepper, spinach, vegan marinara sauce, tomatoes, firm tofu, chickpeas, vegan cheese', 'https://media.istockphoto.com/id/1365127906/photo/protein-rich-lasagna-vegan-italian-food-plant-based-recipe-medit.jpg?s=2048x2048&w=is&k=20&c=RaSMjPx9vS_6i0_mpOC0CdD9LMmkW_NlCWOeqFWYSvg=');
 
 INSERT INTO statuses (status)
 VALUES
